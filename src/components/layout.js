4b23e93261f0c3a21e3219dc863664166e1a01ac
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-02-27 19:23:10 -0800

    fix syntax bug and style tweaks

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ const Layout = ({ children }) => {
         <main>
           {children}
         </main>
-        <footer style={{textAlign: 'center'}}>
+        <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
